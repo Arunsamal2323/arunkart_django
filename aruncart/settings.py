119cfc81c905fd7e38e08f52430910fe74c2fb08
arun <arunsamal4@gmail.com>
2022-05-04 23:37:40 +0530

    setting file commit

--- aruncart/settings.py
+++ aruncart/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'arunsamal4@gmail.com'
-EMAIL_HOST_PASSWORD = 'sanmacsm123'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_PORT = 534
